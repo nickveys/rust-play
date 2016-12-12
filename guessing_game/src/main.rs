c2cb89131c176c521c0c252b1a25723fe4473792
Nick Veys <nick@codelever.com>
2016-12-12 15:30:19 -0600

    Remove secret number from printlns

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -9,7 +9,6 @@ fn main() {
     println!("Please input your guess.");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is: {}", secret_number);
 
     loop {
         let mut guess = String::new();
