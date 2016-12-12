2d44068920c4c56b37a7dc1842be583bf6db7add
Nick Veys <nick@codelever.com>
2016-12-12 15:11:24 -0600

    Add rand create, use it in guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,8 +1,14 @@
+extern crate rand;
+
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);
 
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read line");
