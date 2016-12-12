5c87cdf4cd0badc5d4342da2fe2f777d76e25509
Nick Veys <nick@codelever.com>
2016-12-12 15:23:26 -0600

    Add guess w/loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -10,9 +11,22 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is: {}", secret_number);
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let trimmed = guess.trim();
-    println!("You guessed: {} ({} characters)", trimmed, trimmed.len());
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal   => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
